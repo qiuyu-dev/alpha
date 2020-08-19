01dcf0481f11c10ff76cf3efe395fb1694f94eb4
qiuyu-dev <75387273@qq.com>
2020-08-19 14:39:02 +0800

    fix exception handler

--- src/main/java/com/mysoft/alpha/exception/DefaultExceptionHandler.java
+++ src/main/java/com/mysoft/alpha/exception/DefaultExceptionHandler.java
@@ -3,6 +3,8 @@ package com.mysoft.alpha.exception;
 import com.mysoft.alpha.result.Result;
 import com.mysoft.alpha.result.ResultFactory;
 import org.apache.shiro.authz.UnauthorizedException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,9 +17,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @ControllerAdvice
 @ResponseBody
 public class DefaultExceptionHandler {
+	private static final Logger log = LoggerFactory.getLogger(DefaultExceptionHandler.class);
+	
     @ExceptionHandler(value = Exception.class)
     public Result exceptionHandler(Exception e) {
-        String message = null;
+        String message = "系统错误";
 
         if (e instanceof IllegalArgumentException) {
             message = "传入了错误的参数";
@@ -33,9 +37,10 @@ public class DefaultExceptionHandler {
         
         if (e instanceof CustomException) {
         	CustomException customException = (CustomException) e;
-        	return ResultFactory.buildFailResult(customException.getMsg());
+        	message = customException.getMsg();
         }
-
+        
+        log.error(message,e);
         return ResultFactory.buildFailResult(message);
     }
 }
